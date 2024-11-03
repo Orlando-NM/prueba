bb1c607c0bd1d5befada22d25bb99ddb930949f9
orlando <sonrrisa49@hotmail.es>
2024-11-03 10:22:55 -0500

    objetos de prueba

---
+++ app.js
@@ -0,0 +1,4 @@
+document.write("holaaa");
+console.log('Hola we');
+document.body.innerHTML = "otro mensaje"; /** este reemplaza al write*/
+document.getElementsById("mensaje").innerHTML = "tercer mensaje";
